d1ccd581fd4cce976e62815552d71ce9c52a2bd1
rodrigoaives <rodrigoaives@outlook.com>
2020-07-07 08:05:35 -0300

    Refresh Token working

--- src/helpers/jwt.js
+++ src/helpers/jwt.js
@@ -4,7 +4,7 @@ const jwt = require('jsonwebtoken');
 const tokenPrivateKey = process.env.JWT_TOKEN_PRIVATE_KEY;
 const refreshTokenPrivateKey = process.env.JWT_REFRESH_TOKEN_PRIVATE_KEY;
 
-const options = { expiresIn: '1 minute' };
+const options = { expiresIn: '30 minutes' };
 const refreshOptions = { expiresIn: '30 days' };
 
 const generateJwt = (payload) => {
